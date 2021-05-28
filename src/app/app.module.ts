e860845a0d340f3d09ffc8119c0e3618aa0dc271
HIBERUS\dmesa <dmesa@hiberus.com>
2021-05-28 10:51:32 +0200

    logica implementada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
@@ -10,7 +11,8 @@ import { CalculadoraComponent } from './calculadora/calculadora.component';
     CalculadoraComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SystemJsNgModuleLoader } from '@angular/core';
 
 @Component({
   selector: 'app-calculadora',
@@ -9,7 +9,113 @@ export class CalculadoraComponent implements OnInit {
 
   constructor() { }
 
+  salida:string =" ";
+  salida2:string ="";
+
   ngOnInit(): void {
   }
+
+  anadir1():void{
+    this.salida= this.salida+ '1';
+  }
+  anadir2():void{
+    this.salida= this.salida+ '2';
+  }
+  anadir3():void{
+    this.salida= this.salida+ '3';
+  }
+  anadir4():void{
+    this.salida= this.salida+ '4';
+  }
+  anadir5():void{
+    this.salida= this.salida+ '5';
+  }
+  anadir6():void{
+    this.salida= this.salida+ '6';
+  }
+  anadir7():void{
+    this.salida= this.salida+ '7';
+  }
+  anadir8():void{
+    this.salida= this.salida+ '8';
+  }
+  anadir9():void{
+    this.salida= this.salida+ '9';
+  }
+  anadir0():void{
+    this.salida= this.salida+ '0';
+  }
+  anadirMas():void{
+    this.salida2=this.salida + "+";
+    this.salida= "";
+  }
+  anadirMenos():void{
+    this.salida2=this.salida + "-";
+    this.salida= "";
+  }
+  anadirProducto():void{
+    this.salida2=this.salida + "*";
+    this.salida= "";
+  }
+  anadirDivision():void{
+    this.salida2=this.salida + "/";
+    this.salida= "";
+  }
+  resultado():void{
+    switch(this.salida2.substring(this.salida2.length-1)){
+      case "+":
+        this.suma();
+        break;
+      case "-":
+        this.resta();
+        break;
+      case "*":
+        this.producto();
+        break;
+      case "/":
+        this.division();
+        break;
+      default:
+        alert("MAAAAAAAAAAAAL");
+        break;
+    }
+
+  }
+  borrar():void{
+  this.salida="";
+
+  }
+  borrartodo():void{
+    this.salida="";
+    this.salida2="";
+
+  }
+  private suma():void{
+    let numero1:number=Number(this.salida);
+    let numero2:number=Number(this.salida2.substring(0,this.salida2.length-1));
+    console.log(numero2);
+    
+    this.salida=""+(numero1+numero2);
+    this.salida2 = numero2 + "+" + numero1 + "=" + (numero1 + numero2);
+  }
+  private resta():void{
+    let numero1:number=Number(this.salida);
+    let numero2:number=Number(this.salida2.substring(0,this.salida2.length-1));
+    this.salida=""+ (numero2-numero1);
+    this.salida2 = numero2 + "-" + numero1 + "=" + (numero2 - numero1);
+  }
+  private producto():void{
+    let numero1:number=Number(this.salida);
+    let numero2:number=Number(this.salida2.substring(0,this.salida2.length-1));
+    this.salida=""+(numero1*numero2);
+    this.salida2 = numero2 + "*" + numero1 + "=" + (numero1 * numero2);
+  }
+  private division():void{
+    let numero1:number=Number(this.salida);
+    let numero2:number=Number(this.salida2.substring(0,this.salida2.length-1));
+    this.salida=""+(numero2/numero1);
+    this.salida2 = numero2 + "/" + numero1 + "=" + (numero2 / numero1);
+  }
+  
 
 }
